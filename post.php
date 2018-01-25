b36d9aac338ab57261e5fc5611f22474758fd85d
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-24 20:19:59 -0800

    Users can now send and save comments to the database

--- post.php
+++ post.php
@@ -51,15 +51,43 @@
                     <?php } ?>
 
                 <!-- Blog Comments -->
+                <?php
+                    if(isset($_POST['create_comment'])) {
+                        $post_id = $_GET['p_id'];
+
+                        $comment_author = $_POST['comment_author'];
+                        $comment_email = $_POST['comment_email'];
+                        $comment_content = $_POST['comment_content'];
+
+                        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+
+                        $query .= "VALUES ($post_id,'{$comment_author}','{$comment_email}','{$comment_content}','unapproved',now())";
+
+                        $create_comment_query = mysqli_query($connection, $query);
+                        
+                        if(!$create_comment_query) {
+                            die("Query failed. " . mysqli_error($connection));
+                        }
+                    }
+                ?>
 
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form action="" method="post" role="form">
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                            <label for="author">Author</label>
+                            <input class="form-control" type="text" name="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="email" name="comment_email">
+                        </div>
+                        <div class="form-group">
+                            <label for="comment">Comment</label>
+                            <textarea class="form-control" rows="3" name="comment_content"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
                     </form>
                 </div>
 
