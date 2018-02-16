03abefd4ec6e0c95bab57e9629af0bce45008510
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-15 19:24:12 -0800

    admin navbar is now dynamic

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -2,10 +2,8 @@
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/1.0.0-alpha.2/classic/ckeditor.js"></script>
     <script src="js/scripts.js"></script>
 
