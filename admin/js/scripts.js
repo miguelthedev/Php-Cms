ca241d804f3bcedf0188e728df832ccc03f0ab2b
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-15 19:03:10 -0800

    Added bulk options for posts

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+    $('#selectAllBoxes').click(function(e) {
+        if(this.checked) {
+            $('.checkBoxes').each(function() {
+                this.checked = true;
+            });
+        } else {
+            $('.checkBoxes').each(function() {
+                this.checked = false;
+            });
+        }
+    })
+
+
     //EDITOR
     ClassicEditor.create(document.querySelector('#body')).catch(error => {
         console.error(error);
