b069b1e64a6547ea3138216d1bb7269b1dddb7bf
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-17 21:49:56 -0800

    CMS now keeps track of posts's views count

--- post.php
+++ post.php
@@ -14,17 +14,19 @@
                 <?php
                     if(isset($_GET['p_id'])) {
                         $post_id = $_GET['p_id'];
-                    }
+                        
+                        $view_query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = '$post_id' ";
+                        $send_query = mysqli_query($connection, $view_query);
 
-                    $query = "SELECT * FROM posts WHERE post_id = $post_id";
-                    $select_all_posts_query = mysqli_query($connection, $query);
+                        $query = "SELECT * FROM posts WHERE post_id = $post_id";
+                        $select_all_posts_query = mysqli_query($connection, $query);
 
-                    while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        $post_title = $row['post_title'];
-                        $post_author = $row['post_author'];
-                        $post_date = $row['post_date'];
-                        $post_image = $row['post_image'];
-                        $post_content = $row['post_content'];
+                        while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                            $post_title = $row['post_title'];
+                            $post_author = $row['post_author'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = $row['post_content'];
 
                         ?>
 
@@ -42,7 +44,13 @@
                         <p><?php echo $post_content; ?></p>
 
                         <hr>
-                    <?php } ?>
+                    <?php } 
+                    
+                    } else {
+                        header("Location: index.php");
+                    }
+                    
+                    ?>
 
                 <!-- Blog Comments -->
                 <?php
