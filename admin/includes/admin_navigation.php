06109f26e0a62b3761b5621cac8f3d6f1cbf99dd
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-18 13:34:42 -0800

    Admin page now tracks how many users are online

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -12,6 +12,9 @@
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
         <li>
+            <a href="#">Users Online: <?php echo users_online(); ?></a>
+        </li>
+        <li>
             <a href="../index.php">Site</a>
         </li>
         
