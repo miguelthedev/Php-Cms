2999b9383a0b1ddb1765e794562705502ef9c832
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-18 22:36:24 -0800

    Fixed front page bug where no message appeared if posts weren't available

--- index.php
+++ index.php
@@ -23,44 +23,49 @@
                         $page_1 = ($page * 5) - 5;
                     }
 
-                    $post_query_count = "SELECT * FROM posts";
+
+
+                    $post_query_count = "SELECT * FROM posts WHERE post_status = 'published' ";
                     $find_count = mysqli_query($connection, $post_query_count);
                     $count = mysqli_num_rows($find_count);
-                    $count = ceil($count/5);
 
-                    $query = "SELECT * FROM posts LIMIT $page_1, 5";
-                    $select_all_posts_query = mysqli_query($connection, $query);
+                    if($count < 1) {
+                        echo "<h1 class='text-center'>No posts available.</h1>";
+                    } else {
 
-                    while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        $post_id = $row['post_id'];
-                        $post_title = $row['post_title'];
-                        $post_user = $row['post_user'];
-                        $post_date = $row['post_date'];
-                        $post_image = $row['post_image'];
-                        $post_content = substr($row['post_content'],0,150);
-                        $post_status = $row['post_status'];
+                        $count = ceil($count/5);
 
-                        if($post_status == 'published') {
+                        $query = "SELECT * FROM posts LIMIT $page_1, 5";
+                        $select_all_posts_query = mysqli_query($connection, $query);
 
-                        ?>
+                        while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                            $post_id = $row['post_id'];
+                            $post_title = $row['post_title'];
+                            $post_user = $row['post_user'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = substr($row['post_content'],0,150);
+                            $post_status = $row['post_status'];
 
-                        <!-- First Blog Post -->
-                        <h2>
-                            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
-                        </h2>
-                        <p class="lead">
-                            by <a href="author_posts.php?author=<?php echo $post_user; ?>"><?php echo $post_user; ?></a>
-                        </p>
-                        <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
-                        <hr>
-                        <a href="post.php?p_id=<?php echo $post_id; ?>">
-                            <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                        </a>
-                        <hr>
-                        <p><?php echo $post_content; ?></p>
-                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                            ?>
 
-                        <hr>
+                            <!-- First Blog Post -->
+                            <h2>
+                                <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                            </h2>
+                            <p class="lead">
+                                by <a href="author_posts.php?author=<?php echo $post_user; ?>"><?php echo $post_user; ?></a>
+                            </p>
+                            <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
+                            <hr>
+                            <a href="post.php?p_id=<?php echo $post_id; ?>">
+                                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                            </a>
+                            <hr>
+                            <p><?php echo $post_content; ?></p>
+                            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                            <hr>
                     <?php } } ?>
           
             </div>
